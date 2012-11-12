d3c5b3f208b602021b3b569a6dde2b38fc8fd735
freshlhy <freshlhy@gmail.com>
2012-11-12 16:19:40 +0800

    first commit

---
+++ app/models/permission.rb
@@ -0,0 +1,12 @@
+class Permission < Struct.new(:user)
+  def allow?(controller, action)
+    if user.nil?
+      controller == "topics" && action.in?(%w[index show])
+    elsif user.admin?
+      true
+    else
+      controller == "topics" && action != "destroy"
+    end
+  end
+
+end
